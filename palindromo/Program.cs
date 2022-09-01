7603802b865a681093fb9da54758d19f7b718054
Alberto <albertobarbosa0003@gmail.com>
2022-09-01 20:12:39 -0300

    Adicionar arquivos de projeto.

---
+++ palindromo/Program.cs
@@ -0,0 +1,18 @@
+﻿int escolha;
+do
+{
+    Console.WriteLine("Vamos inverter a palavra digitada");
+    string palavra = Console.ReadLine();
+    string PalavraInvertida = " ";
+
+    for(int i = palavra.Length - 1; i >= 0; i --)
+    {
+        PalavraInvertida += palavra[i];
+    }
+
+    Console.WriteLine("E a palavra invertida é:");
+    Console.WriteLine(PalavraInvertida);
+
+    Console.WriteLine("Digite 0 para parar o programa ou 1 para continuar");
+    escolha = int.Parse(Console.ReadLine());
+} while (escolha != 0);
